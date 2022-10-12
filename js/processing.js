1edf47a40f19fcd3bcb35167df66412659ac627f
Lillian Duarte <lidu5648@colorado.edu>
2022-10-12 10:41:40 -0600

    step 2 done: read through js files line by line and print out all of a specific character's lines

--- js/processing.js
+++ js/processing.js
@@ -1,14 +1,49 @@
 
-// Requiring fs module in which 
-// readFile function is defined.
-const fs = require('fs')
-  
-// Reading data in utf-8 format
-// which is a type of character set.
-// Instead of 'utf-8' it can be 
-// other character set also like 'ascii'
+// run with node processing.js
+
+const fs = require('fs');
+const readline = require("readline");
+const name = "KING.";
+
+
 fs.readFile('../textfiles/hamlet.txt', 'utf-8', (err, data) => {
+
     if (err) throw err;
-  
-    console.log(data);
+    // console.log(data);
+    const input_path = "../textfiles/hamlet.txt";
+    const inputStream = fs.createReadStream(input_path);
+
+
+    const output_path = "hamletoutput.txt";
+    const outputStream = fs.createWriteStream(output_path, { encoding: "utf8" });
+
+    var lineReader = readline.createInterface({
+    input: inputStream,
+    terminal: false,
+    });
+
+    let myflag = false;
+    lineReader.on("line", function (line) {
+        const myArray = line.split(" ");        
+
+        if(myArray[0] == name)
+        {
+            myflag = true;
+        }
+        else
+        {
+            if(myArray[0].toUpperCase() == myArray[0] && myArray[0].length > 1)
+            {
+                myflag = false;
+            }
+        }
+
+        if(myflag == true)
+        {
+            outputStream.write(line + "\n");
+        }
+        
+    });
+
 })
+
